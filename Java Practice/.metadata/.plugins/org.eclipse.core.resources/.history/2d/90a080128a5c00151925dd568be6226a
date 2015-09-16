b115bcf6d6478be832cb2b277043e2af12e927d6
abdalimran <abdalimran@gmail.com>
2015-09-16 22:41:26 +0600

    16/09/2015

---
+++ Java Practice/.metadata/.plugins/org.eclipse.core.resources/.history/2d/90a080128a5c00151925dd568be6226a
@@ -0,0 +1,5 @@
+package java.Class;
+
+public class Java_Class {
+	
+}
---
+++ Java Practice/Java_Basics/src/imran/java/basics/Main.java
@@ -0,0 +1,9 @@
+package imran.java.basics;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
